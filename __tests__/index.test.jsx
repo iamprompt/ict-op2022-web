e739af5f6c6e69b41deaee5b8ffbc9926ec842c2
itsmebabysmiley <54635089+itsmebabysmiley@users.noreply.github.com>
2022-11-16 20:31:15 +0700

    branch and stmts coverage passed 75%

--- __tests__/index.test.jsx
+++ __tests__/index.test.jsx
@@ -3,10 +3,6 @@ import '@testing-library/jest-dom'
 
 import {checkAnswer} from 'src/modules/api/services/activity/checkAnswer'
 import {getQuestQuestion} from 'src/modules/api/services/activity/getQuestQuestion'
-import {getLineUserFromIdToken} from 'src/modules/external/line'
-import QuestLog from 'src/modules/mongoose/models/questlog.model'
-
-const mockingoose = require('mockingoose');
 
 describe('register-unit-tests.ts',() => {
 
@@ -15,6 +11,7 @@ describe('register-unit-tests.ts',() => {
    */
   it('testCheckAnswer', async () => {
     expect(checkAnswer("123",'123')).true;
+    expect(checkAnswer(["123","234"],["123","234"])).true;
   });
 
   /**
@@ -24,10 +21,27 @@ describe('register-unit-tests.ts',() => {
     const q = await getQuestQuestion(2,'th',false,false);
     const id = q[0].id;
     expect(id).toBe("Q1");
+
+    let q5 = await getQuestQuestion(5,'th',false,false);
+    let id5 = q5[0].id;
+    expect(id5).toBe("Q5");
+
+    q5 = await getQuestQuestion(5,'th',true,true);
+    id5 = q5[0].id;
+    expect(id5).toBe("Q5");
+
+    q5 = await getQuestQuestion(3);
+
+    try {
+        await getQuestQuestion(13,'th',true,true)
+    } catch (e) {
+        expect(e.message).toBe("Question not found");
+    }
+
   })
 
   it('test mocking', async () => {
-    
+
   });
 })
 export {};
