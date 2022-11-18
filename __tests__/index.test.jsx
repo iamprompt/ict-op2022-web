b284cb1527d096db3e76b8587bf8653998cb4178
itsmebabysmiley <54635089+itsmebabysmiley@users.noreply.github.com>
2022-11-18 14:09:32 +0700

    added comment

--- __tests__/index.test.jsx
+++ __tests__/index.test.jsx
@@ -8,6 +8,11 @@ describe('register-unit-tests.ts', () => {
 
   /**
    * Test check answer.
+   *  checkAnserwer(expectedAnswer, answer) uses to check between two strings are the same or not.
+   * If thery are the same, it will return true. Otherwise, it will return false.
+   * 
+   * For this test case, we apply ISP technique. In result, it can achive high percentage of 
+   * coverage.
    */
   it('testCheckAnswer', async () => {
     expect(checkAnswer('MUICT', 'MUICT')).true;
@@ -19,6 +24,18 @@ describe('register-unit-tests.ts', () => {
 
   /**
    * Test get quest question.
+   *  getQuestQuestion(questNo, lang, question, answer) will get a question according to questNo.
+   * It will form a question as well by call formquestion(). This function will return an object of
+   * question including:
+   *  - id
+   *  - type
+   *  - questNo
+   *  - questTitle
+   *  - question
+   *  - expectedAnswer
+   * 
+   * In this testcase, we combined ISP and logic coverage technique to acheive 75% coverage.
+   *
    */
   it('testGetQuestQuestion', async () => {
     let expectedOutput = [{
@@ -72,7 +89,7 @@ describe('register-unit-tests.ts', () => {
       type: 'TEXT_FIELD',
       questNo: 2,
       questTitle: 'Meet & Greet Zone',
-      question: 'จำนวนชมรม (Club) ที่จัดแสดงมีกี่ชมรม', 
+      question: 'จำนวนชมรม (Club) ที่จัดแสดงมีกี่ชมรม',
     }];
 
     expect(await getQuestQuestion(2, 'th', true, false)).toMatchObject(expectedOutput)
@@ -87,34 +104,69 @@ describe('register-unit-tests.ts', () => {
     }];
 
     expect(await getQuestQuestion(2, 'th', false, true)).toMatchObject(expectedOutput)
-    await getQuestQuestion(5,'th',false,false);
-    await getQuestQuestion(3);
-    //  const id = q[0].id;
-    //  expect(id).toBe("Q1");
 
-    //  let q5 = await getQuestQuestion(5,'th',false,false);
-    //  let id5 = q5[0].id;
-    //  expect(id5).toBe("Q5");
+    expectedOutput = [
+      {
+        id: 'Q5',
+        type: 'MULTIPLE_CHOICE',
+        questNo: 5,
+        questTitle: 'Innovative Projects Zone',
+        choices: {
+          'v-achilles': 'V-Achilles',
+          mosquito: 'Mosquito',
+          'vr-xylophone': 'VR Xylophone',
+          midjourney: 'Midjourney',
+          psimilan: 'PSIMILAN',
+          'food-spoilage': 'Food Spoilage',
+          airadar: 'Airadar',
+          'dall-e': 'DALL-E',
+          'web-audit-tool': 'Web Audit Tool',
+          microusity: 'Microusity',
+          'mu-blink-analyzer': 'MU Blink Analyzer',
+          tpt: 'TPT',
+          'receipt-recognizer': 'Receipt Recognizer',
+          isit: 'iSit',
+          'suture-bot': 'Suture Bot',
+          'cof-learn': 'Cof-Learn',
+          ocr: 'OCR',
+          wabiqa: 'WabiQA',
+          ezfit: 'EzFIT',
+          'fixme-bot': 'FixMe Bot',
+          mirai: 'Mirai',
+          'smart-color': 'Smart Color',
+          esit: 'eSit',
+          landsage: 'LandSage',
+          automl: 'AutoML',
+          orchidator: 'Orchidator',
+          'github-autopilot': 'Github Autopilot',
+          'self-driving-car': 'Self-driving car',
+          gaifa: 'GAIFA',
+          'stable-diffusion': 'Stable Diffusion',
+          'air-quality-monitoring': 'Air Quality Monitoring',
+          whitedefender: 'WhiteDefender'
+        }
+      }
+    ]
 
-    //  q5 = await getQuestQuestion(5,'th',true,true);
-    //  id5 = q5[0].id;
-    //  expect(id5).toBe("Q5");
+    expect(await getQuestQuestion(5, 'th', false, false)).toMatchObject(expectedOutput);
 
-    //  q5 = await getQuestQuestion(3);
-
-    //  try {
-    //      await getQuestQuestion(13,'th',true,true)
-    //  } catch (e) {
-    //      expect(e.message).toBe("Question not found");
-    //  }
+    expectedOutput = [
+      {
+        id: 'Q2',
+        type: 'TEXT_FIELD',
+        questNo: 3,
+        questTitle: 'International Experiences Zone',
+        question: 'ประเทศที่มีจำนวน Exchange Students มากที่สุดคือประเทศใด'
+      }
+    ]
     
+    expect(await getQuestQuestion(3)).toMatchObject(expectedOutput);
+
+
   })
 
-  it('test mocking', async () => {
-    // const q = QUESTIONS.filter(element => {
-    //   console.log(element)
-    // });
-    console.log(QUESTIONS.length);
+  it('unit test3', async () => {
+
   });
 })
 export { };
